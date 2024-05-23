07b6a2f37b746ac3b8cbf9aeba3057de758c17e9
unknown <haithanh.uit@gmail.com>
2024-05-23 16:17:55 +0700

    clear project unuse file

--- src/main/java/org/rabbitmqproject/RabbitmqProject.java
+++ src/main/java/org/rabbitmqproject/RabbitmqProject.java
@@ -47,17 +47,17 @@ public class RabbitmqProject {
             }            
             new consumer(uri);
             new producer(uri);
-        int port = 8080; // Port for the health check endpoint
+            int port = 8080; // Port for the health check endpoint
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.createContext("/health", (exchange -> {
-            String response = "{\"status\":\"UP\"}";
-            exchange.sendResponseHeaders(200, response.getBytes().length);
-            OutputStream output = exchange.getResponseBody();
-            output.write(response.getBytes());
-            output.flush();
-            exchange.close();
-        }));
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+            server.createContext("/health", (exchange -> {
+                String response = "{\"status\":\"UP\"}";
+                exchange.sendResponseHeaders(200, response.getBytes().length);
+                OutputStream output = exchange.getResponseBody();
+                output.write(response.getBytes());
+                output.flush();
+                exchange.close();
+            }));
 
         // Start the server
         server.start();
@@ -76,74 +76,74 @@ public class RabbitmqProject {
         System.out.println("Finish test run");
     }
     
-    public void producer(){
-        try {
-            System.out.println("Start Producer------------------------");
-            String uri = "amqps://rakkoayb:bLYhhjRY66XIbXOhGSCTNWMX5S1RHzhS@armadillo.rmq.cloudamqp.com/rakkoayb";
-            ConnectionFactory factory = new ConnectionFactory();
-//            factory.setHost("localhost");
-            factory.setUri(uri);
-            Connection connection =  factory.newConnection();
-            Channel channel = connection.createChannel();
-            String nameQueue = "product_list";            
-            String time = String.valueOf(new Date().getTime());
-            String msg = "HELLO HELLO "+time;
-            channel.queueDeclare(nameQueue, false, false, false, null);
-            for(int i = 0; i < 30; i++){
-                time = String.valueOf(new Date().getTime());
-                msg = "HELLO HELLO ["+i+"] "+time;
-                channel.basicPublish("", nameQueue, null, msg.getBytes());
-                System.out.println("SEND TO QUEUE: "+msg);
-                Thread.sleep(1000);
-            }
-            System.out.println("connection_id: "+connection.getId());
-            Thread.sleep(50000);
-            channel.close();
-            connection.close();
-        } catch (Exception ex) {
-//            Logger.getLogger(RabbitmqProject.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    public void consumer(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    System.out.println("Start consumer------------------------");
-                    String nameQueue = "product_list";
-//                    String msg = "HELLO HELLO";
-                    String uri = "amqps://rakkoayb:bLYhhjRY66XIbXOhGSCTNWMX5S1RHzhS@armadillo.rmq.cloudamqp.com/rakkoayb";
-                    ConnectionFactory factory = new ConnectionFactory();
-        //            factory.setHost("localhost");
-                    factory.setUri(uri);
-                    Connection connection =  factory.newConnection();
-                    Channel channel = connection.createChannel();
-                    channel.queueDeclare(nameQueue, false, false, false, null);
-                    ////////////////////////////////////////
-                    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-                    DefaultConsumer consumer = new DefaultConsumer(channel) {
-                        @Override
-                         public void handleDelivery(
-                            String consumerTag,
-                            Envelope envelope, 
-                            AMQP.BasicProperties properties, 
-                            byte[] body) throws IOException {
-                                String message = new String(body, "UTF-8");
-                                String time = String.valueOf(new Date().getTime());
-                                System.out.println("Receiver MSG["+time+"]: "+message);
-                                // process the message
-                         }
-                    };
-                    channel.basicConsume(nameQueue, true, consumer);                    
-//                    channel.basicPublish("", nameQueue, null, msg.getBytes());
-                    System.out.println("connection_id: "+connection.getId());
-                    Thread.sleep(30000);
-                    channel.close();
-                    connection.close();
-                } catch (Exception ex) {
-        //            Logger.getLogger(RabbitmqProject.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        }).start();
-    }
+//    public void producer(){
+//        try {
+//            System.out.println("Start Producer------------------------");
+//            String uri = "amqps://rakkoayb:bLYhhjRY66XIbXOhGSCTNWMX5S1RHzhS@armadillo.rmq.cloudamqp.com/rakkoayb";
+//            ConnectionFactory factory = new ConnectionFactory();
+////            factory.setHost("localhost");
+//            factory.setUri(uri);
+//            Connection connection =  factory.newConnection();
+//            Channel channel = connection.createChannel();
+//            String nameQueue = "product_list";            
+//            String time = String.valueOf(new Date().getTime());
+//            String msg = "HELLO HELLO "+time;
+//            channel.queueDeclare(nameQueue, false, false, false, null);
+//            for(int i = 0; i < 30; i++){
+//                time = String.valueOf(new Date().getTime());
+//                msg = "HELLO HELLO ["+i+"] "+time;
+//                channel.basicPublish("", nameQueue, null, msg.getBytes());
+//                System.out.println("SEND TO QUEUE: "+msg);
+//                Thread.sleep(1000);
+//            }
+//            System.out.println("connection_id: "+connection.getId());
+//            Thread.sleep(50000);
+//            channel.close();
+//            connection.close();
+//        } catch (Exception ex) {
+////            Logger.getLogger(RabbitmqProject.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+//    public void consumer(){
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    System.out.println("Start consumer------------------------");
+//                    String nameQueue = "product_list";
+////                    String msg = "HELLO HELLO";
+//                    String uri = "amqps://rakkoayb:bLYhhjRY66XIbXOhGSCTNWMX5S1RHzhS@armadillo.rmq.cloudamqp.com/rakkoayb";
+//                    ConnectionFactory factory = new ConnectionFactory();
+//        //            factory.setHost("localhost");
+//                    factory.setUri(uri);
+//                    Connection connection =  factory.newConnection();
+//                    Channel channel = connection.createChannel();
+//                    channel.queueDeclare(nameQueue, false, false, false, null);
+//                    ////////////////////////////////////////
+//                    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+//                    DefaultConsumer consumer = new DefaultConsumer(channel) {
+//                        @Override
+//                         public void handleDelivery(
+//                            String consumerTag,
+//                            Envelope envelope, 
+//                            AMQP.BasicProperties properties, 
+//                            byte[] body) throws IOException {
+//                                String message = new String(body, "UTF-8");
+//                                String time = String.valueOf(new Date().getTime());
+//                                System.out.println("Receiver MSG["+time+"]: "+message);
+//                                // process the message
+//                         }
+//                    };
+//                    channel.basicConsume(nameQueue, true, consumer);                    
+////                    channel.basicPublish("", nameQueue, null, msg.getBytes());
+//                    System.out.println("connection_id: "+connection.getId());
+//                    Thread.sleep(30000);
+//                    channel.close();
+//                    connection.close();
+//                } catch (Exception ex) {
+//        //            Logger.getLogger(RabbitmqProject.class.getName()).log(Level.SEVERE, null, ex);
+//                }
+//            }
+//        }).start();
+//    }
 }
